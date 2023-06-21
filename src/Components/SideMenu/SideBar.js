24405d38728fde5a3397514e7614ff1121719efc
ariericyron <ariericyron@gmail.com>
2023-06-21 23:41:05 +0300

    feat:implemented side menu and its icons

--- src/Components/SideMenu/SideBar.js
+++ src/Components/SideMenu/SideBar.js
@@ -1,7 +1,49 @@
+import { AppstoreAddOutlined, BarChartOutlined, CalendarOutlined, LineChartOutlined, WarningOutlined } from '@ant-design/icons'
+import { Menu } from 'antd'
 import React from 'react'
 
 export default function SideBar() {
   return (
-    <div className='SideBar'>SideBar</div>
+    <div className='SideBar'>
+      <Menu
+      items={[
+        {
+          label: "Dashboard",
+          icon: <AppstoreAddOutlined />,
+          key: "/"
+        },
+        {
+          label: "Goal Timeline",
+          icon: <BarChartOutlined />,
+          key: "/"
+        },
+        {
+          label: "Daily Routine",
+          icon: <CalendarOutlined />,
+          key: "/"
+        },
+        {
+          label: "Weekly Routine",
+          icon: <CalendarOutlined />,
+          key: "/"
+        },
+        {
+          label: "Monthly Routine",
+          icon: <CalendarOutlined />,
+          key: "/"
+        },
+        {
+          label: "Report",
+          icon: <LineChartOutlined />,
+          key: "/"
+        },
+        {
+          label: "Help Centre",
+          icon: <WarningOutlined />,
+          key: "/"
+        },
+      ]}
+      ></Menu>
+    </div>
   )
 }
